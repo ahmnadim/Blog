b1eea10b2f20286c9040a9fe7ff64c550aff67cf
Ahmad Ul Hoq Nadim <ahmadulhoqnadim@gmail.com>
2018-11-05 12:35:47 +0600

    Many to  Many relation between category, tag, post and user

--- app/User.php
+++ app/User.php
@@ -28,7 +28,13 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function role(){
+    public function role()
+    {
         return $this->belongsTo('App\Role');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
     }
 }
