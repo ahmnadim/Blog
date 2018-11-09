4c18f4619bfd671e8da365aa9a1c0d755c453b8a
Ahmad Ul Hoq Nadim <ahmadulhoqnadim@gmail.com>
2018-11-09 22:45:53 +0600

    Notification Module== Author's post Approvel mail to Admin completed.

--- app/Http/Controllers/Author/PostController.php
+++ app/Http/Controllers/Author/PostController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Author;
 
 use App\Category;
 use App\Http\Controllers\Controller;
+use App\Notifications\NewAuthorPost;
 use App\Post;
 use App\Tag;
+use App\User;
 use Brian2694\Toastr\Facades\Toastr;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 
@@ -94,6 +97,9 @@ class PostController extends Controller
 
         $post->categories()->attach($request->categories);
         $post->tags()->attach($request->tags);
+
+        $users = User::where('role_id','1')->get();
+        Notification::send($users, new NewAuthorPost($post));
 
         Toastr::success('Post Added Successfully...', 'success');
         return redirect()->route('author.post.index');
